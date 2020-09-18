f4adaf73b2b8ee441b0b0f930ea97be6e6c2710b
131170n <ori@thebittons.net>
2020-09-18 14:20:30 +0300

    commit

--- screens/Confirm.js
+++ screens/Confirm.js
@@ -1,30 +1,31 @@
-import { View } from 'antd-mobile';
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import 'react-native-gesture-handler';
+import { createStackNavigator } from '@react-navigation/stack';
+import ImagePreview from 'react-native-image-preview';
+const Stack = createStackNavigator();
+let visible = true;
+export default function App(photo, { navigation }) {
 
-export default function Confirm({ navigation }) {
-
-    // const MyOnPressConfirm = () => {
-    //     return (
-    //         navigation.navigate('Weardrobe', { name: 'Weardrobe' })
-    //     );
-    // }
-    // const MyOnPressDecline = () => {
-    //     return (
-    //         navigation.navigate('addClothes', { name: 'addClothes' })
-    //     );
-    // }
+    const MyOnPressConfirm = () => {
+        return (
+            navigation.navigate('Weardrobe', { name: 'Weardrobe' })
+        );
+    }
+    const MyOnPressDecline = () => {
+        return (
+            navigation.navigate('addClothes', { name: 'addClothes' })
+        );
+    }
     return (
         <View style={styles.container}>
-            <Text>kuku</Text>
-            {/* <Image source={photo} style={styles.backgroundImage} />
             <TouchableOpacity style={styles.button} onPress={() => MyOnPressConfirm()}>
                 <Text style={styles.buttonText}>Confirm?</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.button} onPress={() => MyOnPressDecline()}>
                 <Text style={styles.buttonText}>Retake</Text>
-            </TouchableOpacity> */}
+            </TouchableOpacity>
         </View>
     );
 
@@ -46,9 +47,5 @@ const styles = StyleSheet.create({
     buttonText: {
         fontSize: 15,
         color: '#fff',
-    },
-    backgroundImage: {
-        flex: 1,
-        resizeMode: 'cover', // or 'stretch'
     },
 });
